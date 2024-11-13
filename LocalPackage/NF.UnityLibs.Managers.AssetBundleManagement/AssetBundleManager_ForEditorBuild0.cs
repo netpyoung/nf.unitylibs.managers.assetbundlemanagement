3a2db4773536a59698cd67cad0e07fae79b8544b
netpyoung <netpyoung@gmail.com>
2024-11-14 06:22:13 +0900

    refactor: AssetBundleManager_ForEditorBuild0.cs

--- LocalPackage/NF.UnityLibs.Managers.AssetBundleManagement/AssetBundleManager_ForEditorBuild0.cs
+++ LocalPackage/NF.UnityLibs.Managers.AssetBundleManagement/AssetBundleManager_ForEditorBuild0.cs
@@ -10,15 +10,23 @@ namespace NF.UnityLibs.Managers.AssetBundleManagement
     public class AssetBundleManager_ForEditorBuild0 : AssetBundleManager
     {
         public const string OUT_BASE_DIR_NAME = "__ASSETBUNDLE";
+        public const string MANIFEST_NAME = OUT_BASE_DIR_NAME;
 
-        public override async Task<Exception?> InitAsync(string deviceBaseDirFpath, string manifestName)
+        public static string GetDeviceBaseDirFpath_ForEditorBuild0()
         {
             string outBaseDirName = OUT_BASE_DIR_NAME;
             string outBaseBuildDirFPath = Path.GetFullPath(Path.Combine(Application.dataPath, "..", outBaseDirName));
             BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
             int version = 0;
-            string outBuildDirFPath = Path.Combine(outBaseBuildDirFPath, buildTarget.ToString(), version.ToString()).Replace('\\', '/');
-            Exception? ret = await base.InitAsync(outBuildDirFPath, outBaseDirName);
+            string deviceBaseDirFpath = Path.Combine(outBaseBuildDirFPath, buildTarget.ToString(), version.ToString()).Replace('\\', '/');
+            return deviceBaseDirFpath;
+        }
+
+        public override async Task<Exception?> InitAsync(string deviceBaseDirFpath, string manifestName)
+        {
+            deviceBaseDirFpath = GetDeviceBaseDirFpath_ForEditorBuild0();
+            manifestName = MANIFEST_NAME;
+            Exception? ret = await base.InitAsync(deviceBaseDirFpath, manifestName);
             return ret;
         }
     }
