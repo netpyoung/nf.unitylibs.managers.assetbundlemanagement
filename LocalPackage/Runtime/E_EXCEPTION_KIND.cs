9fcf57f058cd8fd42e891e45149678c85c430697
netpyoung <netpyoung@users.noreply.github.com>
2024-11-13 21:52:12 +0000

    deploy: 7c1f27f43b693e575b00052f0a77ef7e6166ce94

--- LocalPackage/Runtime/E_EXCEPTION_KIND.cs
+++
@@ -1,8 +0,0 @@
-namespace NF.UnityLibs.Managers.AssetBundleManagement
-{
-    public enum E_EXCEPTION_KIND
-    {
-        NONE = 0,
-        ERR_ON_INITIALIZE = 100,
-    }
-}
--- LocalPackage/Runtime/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("NF.UnityLibs.Managers.AssetBundleManagement.Tests")]
